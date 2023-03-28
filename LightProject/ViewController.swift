21445e9db19e07f384ab35787078155884f05828
Alyssa Scalia <a.scalia@setonhill.edu>
2023-03-28 15:52:12 -0400

    Initial Commit

---
+++ LightProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LightProject
+//
+//  Created by Alyssa Scalia on 3/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
